67f06159446691c24c890c0b03c76a3e9f522aa0
my-tricks <silva.jeremie93@gmail.com>
2024-08-12 16:46:22 +0200

    mep updates

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,12 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
